ba187d12db1c987c560a206f47eef0076f02c60b
Emily Leffler Schulman <emily.lefflerschulman@umuc.edu>
2019-08-05 16:49:09 -0400

    Add initial plugin class. Add JS media uploader.

---
+++ simple-before-and-after.php
@@ -0,0 +1,227 @@
+<?php
+/**
+ * Plugin Name:         Simple Before and After
+ * Plugin URI:
+ * Description:         Displays a simple grid that swaps Before and After images.
+ * Version:             0.1.0
+ * Requires at least:   5.2
+ * Requires PHP:        7.2
+ * Author:              Emily Leffler Schulman, Mobtown Studios
+ * Author URI:          https://emilylefflerschulman.com
+ * License:             GPLv2 or later
+ * License URI:         https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:         simple-before-and-after
+ */
+
+defined( 'ABSPATH' ) or die( 'Nope!' );
+
+if ( ! class_exists( 'Simple_Before_And_After' ) ) {
+    class Simple_Before_And_After {
+        private $total_posts_to_show;
+
+    	public function __construct() {
+            add_action( 'init', array( $this, 'set_total_posts_to_show' ) );
+            add_action( 'init', array( $this, 'register_before_and_after_post_type' ) );
+            add_action( 'add_meta_boxes', array( $this, 'add_before_and_after_meta_boxes' ) );
+            add_action( 'save_post_before_and_after', array( $this, 'save_before_and_after' ) );
+            add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_admin_scripts' ) );
+            add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_scripts' ) );
+
+            register_activation_hook( __FILE__, array( $this, 'plugin_activate' ) );
+            register_deactivation_hook( __FILE__, array( $this, 'plugin_deactivate' ) );
+        }
+
+        public function plugin_activate() {
+            $this->register_before_and_after_post_type();
+            flush_rewrite_rules();
+        }
+
+        public function plugin_deactivate() {
+            flush_rewrite_rules();
+        }
+
+        public function enqueue_scripts() {
+
+        }
+
+        public function enqueue_admin_scripts() {
+            global $typenow;
+
+    		if( $typenow == 'before_and_after' ) {
+    			wp_enqueue_media();
+    			wp_register_script( 'sba-meta-box-image-loader', plugin_dir_url( __FILE__ ) . '/assets/js/sba-media.js', array( 'jquery' ) );
+    			wp_localize_script( 'sba-meta-box-image-loader', 'meta_image',
+    				array(
+    					'title'        => __( 'Choose or Upload Media', 'simple-before-and-after' ),
+    					'button'       => __( 'Use this media', 'simple-before-and-after' ),
+    				)
+    			);
+    			wp_enqueue_script( 'sba-meta-box-image-loader' );
+    		}
+        }
+
+        public function set_total_posts_to_show() {
+            $this->total_posts_to_show = apply_filters( 'sba_total_posts_to_show', 6 );
+        }
+
+        /**
+        * register_before_and_after_post_type
+        *
+        * Registers Before and After custom post type.
+        *
+        **/
+        public function register_before_and_after_post_type() {
+            $labels = array(
+        		'name'                => __( 'Before and Afters', '' ),
+        		'singular_name'       => __( 'Before and After', '' ),
+        		'add_new'             => __( 'Add New Before and After', '' ),
+        		'add_new_item'        => __( 'Add New Before and After', '' ),
+        		'edit_item'           => __( 'Edit Before and After', '' ),
+        		'new_item'            => __( 'New Before and After', '' ),
+        		'all_items'           => __( 'All Before and Afters', '' ),
+        		'view_item'           => __( 'View Before and After', '' ),
+        		'search_items'        => __( 'Search Before and Afters', '' ),
+        		'not_found'           => __( 'No Before and Afters Found', '' ),
+        		'not_found_in_trash'  => __( 'No Before and Afters Found in Trash', '' ),
+        		'parent_item_colon'   => __( '', '' ),
+        		'menu_name'           => __( 'Before and Afters', '' )
+        	);
+
+        	$args = array(
+        		'label'               => __( 'Before and Afters', '' ),
+        		'labels'              => $labels,
+        		'description'         => '',
+        		'public'              => true,
+        		'publicly_queryable'  => true,
+        		'show_ui'             => true,
+        		'show_in_rest'        => false,
+        		'rest_base'           => '',
+        		'has_archive'         => false,
+        		'show_in_menu'        => true,
+        		'show_in_nav_menus'   => false,
+        		'exclude_from_search' => true,
+        		'capability_type'     => 'post',
+        		'map_meta_cap'        => true,
+        		'hierarchical'        => false,
+        		'rewrite'             => array( 'slug' => 'before_and_after', 'with_front' => true ),
+        		'query_var'           => true,
+        		'menu_position'       => 7,
+        		'supports'            => array( 'title' )
+        	);
+
+        	register_post_type( 'before_and_after', $args );
+        }
+
+        public function add_before_and_after_meta_boxes() {
+            add_meta_box(
+                'before_and_after_meta_box',
+                'Before and After Information',
+                array( $this, 'show_before_and_after_meta_box' ),
+                'before_and_after',
+                'normal',
+                'default'
+            );
+        }
+
+        public function show_before_and_after_meta_box( $post ) {
+            wp_nonce_field( basename( __FILE__), 'before_and_after_nonce_field' );
+
+            $before_img = get_post_meta( $post->ID, 'sba_before_img', true );
+            $after_img = get_post_meta( $post->ID, 'sba_after_img', true );
+
+            ?>
+            <p>Select or upload your images.</p>
+            <div class="field-container">
+                <div class="field">
+                    <label for="sba_before_img"><?php _e( 'Before', 'simple-before-and-after' )?></label><br>
+                    <input type="url" class="large-text" name="sba_before_img" id="sba_before_img" value="<?php echo esc_attr( $before_img ); ?>"><br>
+                    <button type="button" class="button" id="sba_before_img_upload_btn" data-media-uploader-target="#sba_before_img"><?php _e( 'Upload Image', 'simple-before-and-after' )?></button>
+                </div><br>
+                <div class="field">
+                    <label for="sba_after_img"><?php _e( 'After', 'simple-before-and-after' )?></label><br>
+                    <input type="url" class="large-text" name="sba_after_img" id="sba_after_img" value="<?php echo esc_attr( $after_img ); ?>"><br>
+                    <button type="button" class="button" id="sba_after_img_upload_btn" data-media-uploader-target="#sba_after_img"><?php _e( 'Upload Image', 'simple-before-and-after' )?></button>
+                </div>
+            </div>
+            <?php
+        }
+
+        public function save_before_and_after( $post_id ) {
+            echo "vv";
+            //check for nonce
+            if( ! isset( $_POST['before_and_after_nonce_field'] ) ) {
+                return $post_id;
+            }
+            echo "ww";
+            //verify nonce
+            if( ! wp_verify_nonce( $_POST['before_and_after_nonce_field'], basename( __FILE__) ) ) {
+                return $post_id;
+            }
+            echo "yy";
+            //check for autosave
+            if( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) {
+                return $post_id;
+            }
+            echo "zz";
+
+            $before_img = get_post_meta( $post_id, 'sba_before_img', true );
+            $after_img = get_post_meta( $post_id, 'sba_after_img', true );
+
+            update_post_meta( $post_id, 'sba_before_img', $before_img );
+            update_post_meta( $post_id, 'sba_after_img', $after_img );
+        }
+
+        public function get_before_and_after_grid( $args = '' ) {
+            $default_args = array(
+                'before_and_after_id'           => '',
+                'number_of_before_and_afters'   => $this->sba_total_posts_to_show
+            );
+
+            if( ! empty( $args ) && is_array( $args ) ) {
+                foreach( $args as $arg_key => $arg_value ) {
+                    if( array_key_exists( $arg_key, $default_args ) ){
+                        $default_args[$arg_key] = $arg_val;
+                    }
+                }
+            }
+
+            $query_args = array(
+                'post_type'             => 'before_and_after',
+                'posts_per_page'        => $default_args['number_of_before_and_afters'],
+                'post_status'           => 'publish',
+                'no_found_rows'         => true,
+                'update_post_term_cache'=> false
+            );
+
+            // If we only passed in one post ID
+            if(!empty($default_args['before_and_after_id'])){
+                $query_args['include'] = $default_args['before_and_after_id'];
+            }
+
+            $html = '';
+            $before_and_afters = new WP_Query( $query_args );
+
+            if( ! empty( $before_and_afters ) ) {
+                $html .= '<div class="sba-grid-wrapper">';
+
+                    foreach ( $before_and_afters as $before_and_after ) {
+                        $html .= '<div class="sba-grid-item">';
+
+                        $sba_id = $before_and_after->ID;
+                        $sba_before_url = get_post_meta( $sba_id, 'sba_before_img', true );
+
+                        if( ! empty( $sba_before_url ) ) {
+                            $sba_before_id = attachment_url_to_postid( $sba_before_url );
+                            $html .= wp_get_attachment_image( $sba_before_id, array( '465', '180' ), false, array( 'class' => 'sba-grid-item-before-img' ) );
+                        }
+
+                        $html .= '</div>'; // .sba-grid-item
+                    }
+
+                $html .= '</div>'; // .sba-grid-wrapper
+            }
+        }
+    }
+}
+
+$simple_before_and_after = new Simple_Before_And_After;
