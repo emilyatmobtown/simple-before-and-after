5fe2267013d82bf14ab547816ac1db8e5e1ace8e
Emily Leffler Schulman <emily.lefflerschulman@umuc.edu>
2019-10-02 14:36:09 -0400

    Refactor setting adding and validation. Add custom before and after labels. Integrate custom settings. Update i18n.

--- inc/classes/Grid.php
+++ inc/classes/Grid.php
@@ -40,8 +40,10 @@ class Grid {
 	public function set_defaults() {
 		$settings       = Settings::get_settings( true );
 		self::$defaults = array(
-			'before_and_after_id'         => '',
-			'number_of_before_and_afters' => $settings['image_total'],
+			'before_and_after_id' => '',
+			'item_total'          => $settings['item_total'],
+			'before_label'        => $settings['before_label'],
+			'after_label'         => $settings['after_label'],
 		);
 	}
 
@@ -58,15 +60,15 @@ class Grid {
 
 		$query_args = array(
 			'post_type'              => 'before_and_after',
-			'posts_per_page'         => $args['number_of_before_and_afters'],
+			'posts_per_page'         => $args['item_total'],
 			'post_status'            => 'publish',
 			'no_found_rows'          => true,
 			'update_post_term_cache' => false,
 		);
 
 		// If we only passed in one post ID
-		if ( ! empty( $default_args['before_and_after_id'] ) ) {
-			$query_args['include'] = $default_args['before_and_after_id'];
+		if ( ! empty( $args['before_and_after_id'] ) ) {
+			$query_args['include'] = $args['before_and_after_id'];
 		}
 
 		$html     = '';
@@ -77,28 +79,36 @@ class Grid {
 
 			while ( $ba_query->have_posts() ) {
 				$ba_query->the_post();
-				$ba_id         = $ba_query->post->ID;
-				$ba_before_url = get_post_meta( $ba_id, 'sba_before_img', true );
-				$ba_after_url  = get_post_meta( $ba_id, 'sba_after_img', true );
+				$post_id      = $ba_query->post->ID;
+				$before_url   = get_post_meta( $post_id, 'sba_before_img', true );
+				$after_url    = get_post_meta( $post_id, 'sba_after_img', true );
+				$before_label = $args['before_label'];
+				$after_label  = $args['after_label'];
 
 				// Must have both Before and After images to display
-				if ( ! empty( $ba_before_url ) && ! empty( $ba_after_url ) ) {
+				if ( ! empty( $before_url ) && ! empty( $after_url ) ) {
 					$html .= '<div class="sba-grid-item-wrapper">';
 					$html .= '<div class="sba-grid-item">';
 
-					$ba_before_id = attachment_url_to_postid( $ba_before_url );
-					$html        .= '<span class="sba-img-caption">';
-					// translators: This is the caption for the Before image when the grid is displayed.
-					$html .= __( 'Before', 'simple-before-and-after' );
-					$html .= '</span>';
-					$html .= wp_get_attachment_image( $ba_before_id, 'sba-grid-image', false, array( 'class' => 'sba-before-img' ) );
+					$before_id = attachment_url_to_postid( $before_url );
 
-					$ba_after_id = attachment_url_to_postid( $ba_after_url );
-					$html       .= '<span class="sba-img-caption inactive">';
-					// translators: This is the caption for the After image when the grid is displayed.
-					$html .= __( 'After', 'simple-before-and-after' );
-					$html .= '</span>';
-					$html .= wp_get_attachment_image( $ba_after_id, 'sba-grid-image', false, array( 'class' => 'sba-after-img inactive' ) );
+					if ( ! empty( $before_label ) ) {
+						$html .= '<span class="sba-img-caption">';
+						$html .= esc_attr( $before_label );
+						$html .= '</span>';
+					}
+
+					$html .= wp_get_attachment_image( $before_id, 'sba-grid-image', false, array( 'class' => 'sba-before-img' ) );
+
+					$after_id = attachment_url_to_postid( $after_url );
+
+					if ( ! empty( $after_label ) ) {
+						$html .= '<span class="sba-img-caption inactive">';
+						$html .= esc_attr( $after_label );
+						$html .= '</span>';
+					}
+
+					$html .= wp_get_attachment_image( $after_id, 'sba-grid-image', false, array( 'class' => 'sba-after-img inactive' ) );
 
 					$html .= '</div>'; // .sba-grid-item
 					$html .= '</div>'; // .sba-grid-item-wrapper
--- inc/classes/Settings.php
+++ inc/classes/Settings.php
@@ -9,6 +9,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 class Settings {
 
 	protected static $defaults;
+	protected static $setting_titles;
 
 	/**
 	 * Return singleton instance of class
@@ -36,13 +37,33 @@ class Settings {
 		add_action( 'admin_init', array( $this, 'add_settings' ) );
 
 		self::set_defaults();
+		self::set_setting_titles();
 	}
 
 	public function set_defaults() {
 		self::$defaults = array(
-			'image_total'  => 6,
+			'item_total'   => 6,
 			'image_width'  => 485,
 			'image_height' => 200,
+			// translators: This is the default caption on the Before image in the grid.
+			'before_label' => __( 'Before', 'simple-before-and-after' ),
+			// translators: This is the default caption on the After image in the grid.
+			'after_label'  => __( 'After', 'simple-before-and-after' ),
+		);
+	}
+
+	public function set_setting_titles() {
+		self::$setting_titles = array(
+			// translators: This is the label for the Max Items in a Grid setting.
+			'item_total'   => __( 'Max Items in a Grid', 'simple-before-and-after' ),
+			// translators: This is the label of the Image Width field on the Settings page.
+			'image_width'  => __( 'Image Width', 'simple-before-and-after' ),
+			// translators: This is the label of the Image Height field on the Settings page.
+			'image_height' => __( 'Image Height', 'simple-before-and-after' ),
+			// translators: This is the default caption on the Before image in the grid.
+			'before_label' => __( 'Before Label', 'simple-before-and-after' ),
+			// translators: This is the default caption on the After image in the grid.
+			'after_label'  => __( 'After Label', 'simple-before-and-after' ),
 		);
 	}
 
@@ -78,9 +99,9 @@ class Settings {
 	 */
 	public function create_settings_page() {
 		add_options_page(
-			// translators: This is the title of the settings page
+			// translators: This is the title of the settings page.
 			__( 'Simple Before and After Settings', 'simple-before-and-after' ),
-			// translators: This is the name of the settings page in the Settings menu
+			// translators: This is the name of the settings page in the Settings menu.
 			__( 'Before and After', 'simple-before-and-after' ),
 			'manage_options',
 			'simple-before-and-after',
@@ -98,7 +119,7 @@ class Settings {
 		<div class="wrap">
 			<h1>
 				<?php
-				// translators: This is the heading of the settings page
+				// translators: This is the heading of the settings page.
 				esc_html_e( 'Simple Before and After Settings', 'simple-before-and-after' );
 				?>
 			</h1>
@@ -126,11 +147,17 @@ class Settings {
 	}
 
 	/**
-	 * Add settings sections and/or settings_fields
+	 * Add settings sections and/or setting fields
 	 *
 	 * @since 0.1.1
 	 */
 	public function add_settings() {
+		// Get the defaults
+		$fields = self::$defaults;
+
+		// Get the titles
+		$titles = self::$setting_titles;
+
 		add_settings_section(
 			'sba_setting_section_1',
 			'',
@@ -138,195 +165,347 @@ class Settings {
 			'simple-before-and-after'
 		);
 
-		add_settings_field(
-			'image_total',
-			// translators: This is the label of the Number of Images field on the Settings page
-			__( 'Number of Images', 'simple-before-and-after' ),
-			array( $this, 'image_total_callback' ),
-			'simple-before-and-after',
-			'sba_setting_section_1'
-		);
+		// Loop through each setting
+		foreach ( $fields as $field_name => $field_value ) {
 
-		add_settings_field(
-			'image_width',
-			// translators: This is the label of the Image Width field on the Settings page
-			__( 'Image Width', 'simple-before-and-after' ),
-			array( $this, 'image_width_callback' ),
-			'simple-before-and-after',
-			'sba_setting_section_1',
-			array( 'label_for' => 'image_width' )
-		);
+			switch ( $field_name ) {
+				case 'item_total':
+				case 'image_height':
+					add_settings_field(
+						$field_name,
+						$titles[ $field_name ],
+						array( $this, $field_name . '_callback' ),
+						'simple-before-and-after',
+						'sba_setting_section_1'
+					);
+					break;
 
-		add_settings_field(
-			'image_height',
-			// translators: This is the label of the Image Height field on the Settings page
-			__( 'Image Height', 'simple-before-and-after' ),
-			array( $this, 'image_height_callback' ),
-			'simple-before-and-after',
-			'sba_setting_section_1'
-		);
+				case 'image_width':
+					add_settings_field(
+						$field_name,
+						$titles[ $field_name ],
+						array( $this, 'text_field_callback' ),
+						'simple-before-and-after',
+						'sba_setting_section_1',
+						array(
+							'name'  => $field_name,
+							'units' => 'px',
+						)
+					);
+					break;
+
+				default:
+					add_settings_field(
+						$field_name,
+						$titles[ $field_name ],
+						array( $this, 'text_field_callback' ),
+						'simple-before-and-after',
+						'sba_setting_section_1',
+						array( 'name' => $field_name )
+					);
+					break;
+			}
+		}
 	}
 
 	/**
-	 * Outputs setting for image width
+	 * Outputs HTML for item total setting
 	 *
 	 * @since 0.1.1
 	 */
-	public function image_total_callback() {
-		$settings    = self::get_settings();
-		$value       = isset( $settings['image_total'] ) ? $settings['image_total'] : '';
-		$placeholder = self::$defaults['image_total'];
+	public function item_total_callback() {
+		self::text_field_callback( array( 'name' => 'item_total' ) );
 		?>
 
-		<input type="text" id="image_total" name="sba_settings[image_total]" placeholder="Default: <?php echo esc_attr( $placeholder ); ?>" value="<?php echo esc_attr( $value ); ?>">
+		<p class="description">This is the maximum number of Before and After items that will appear in a grid. If there are more items than the max, a random selection will be shown. If there are fewer items than the max, then all be shown.</p>
 
 		<?php
 	}
 
 	/**
-	 * Outputs setting for image width
-	 *
-	 * @since 0.1.1
-	 */
-	public function image_width_callback() {
-		$settings    = self::get_settings();
-		$value       = isset( $settings['image_width'] ) ? $settings['image_width'] : '';
-		$placeholder = self::$defaults['image_width'];
-		?>
-
-		<input type="text" id="image_width" name="sba_settings[image_width]" placeholder="Default: <?php echo esc_attr( $placeholder ); ?>" value="<?php echo esc_attr( $value ); ?>"> px
-		<?php
-	}
-
-	/**
-	 * Outputs setting for image height
+	* Outputs HTML for image height setting
 	 *
 	 * @since 0.1.1
 	 */
 	public function image_height_callback() {
-		$settings    = self::get_settings();
-		$value       = isset( $settings['image_height'] ) ? $settings['image_height'] : '';
-		$placeholder = self::$defaults['image_height'];
+		$args = array(
+			'name'  => 'image_height',
+			'units' => 'px',
+		);
+
+		self::text_field_callback( $args );
 		?>
 
-		<input type="text" id="sba_image_height" name="sba_settings[image_height]" placeholder="Default: <?php echo esc_attr( $placeholder ); ?>" value="<?php echo esc_attr( $value ); ?>"> px
 		<p class="description">Changes to the image size will only apply to images uploaded after the setting is saved. To resize older images, regenerate the thumbnails.</p>
 
 		<?php
 	}
 
 	/**
+	 * Outputs HTML for a text field setting
+	 *
+	 * @param array $args
+	 * @since 0.1.1
+	 */
+	public function text_field_callback( $args ) {
+		if ( empty( $args['name'] ) ) {
+			return;
+		}
+
+		$name  = ( ! empty( $args['name'] ) ) ? $args['name'] : '';
+		$units = ( ! empty( $args['units'] ) ) ? $args['units'] : '';
+
+		// Get saved settings
+		$settings = self::get_settings();
+
+		// Set value to an empty string if no setting saved
+		$value = isset( $settings[ $name ] ) ? $settings[ $name ] : '';
+
+		// Set placeholder to default value
+		// translators: This is the label for default values in the text field placeholder, i.e. Default: 485px
+		$placeholder  = __( ' Default', 'simple-before-and-after' );
+		$placeholder .= ': ';
+		$placeholder .= self::$defaults[ $name ];
+
+		// Output HTML
+		?>
+
+		<input type="text" id="sba_<?php echo esc_attr( $name ); ?>" name="sba_settings[<?php echo esc_attr( $name ); ?>]" class="sba_text_field sba_<?php echo esc_attr( $name ); ?>" placeholder="<?php echo esc_attr( $placeholder ); ?>" value="<?php echo esc_attr( $value ); ?>">
+
+		<?php
+
+		// Output HTML for units if not empty
+		if ( ! empty( $units ) ) {
+			?>
+
+			<span class="sba_text_field_units"><?php echo esc_attr( $units ); ?></span>
+
+			<?php
+		}
+	}
+
+	/**
 	 * Sanitizes and sets settings
 	 *
 	 * @param array $settings to be sanitized
+	 * @return array $new_settings sanitized settings
 	 * @since 0.1.1
 	 */
 	public function sanitize_settings( $settings ) {
+		// Get the defaults
+		$fields = self::$defaults;
+
+		// Get the saved settings
 		$new_settings = self::get_settings();
 
-		$validated = self::validate_image_total( $settings['image_total'] );
-		if ( ! $validated ) {
-			$new_settings['image_total'] = '';
-		} elseif ( isset( $settings['image_total'] ) ) {
-			$new_settings['image_total'] = absint( $settings['image_total'] );
+		// Loop through each setting
+		foreach ( $fields as $field_name => $field_value ) {
+
+			// Construct a variable function to validate each input. Function name includes field name, i.e. validate_item_total
+			$validator = 'validate_' . $field_name;
+
+			// Validate the input. Pass the input and the field name to the validator
+			$validated = call_user_func( array( $this, $validator ), $settings[ $field_name ], $field_name );
+
+			// If the input is not validated, set the new setting to an empty string
+			if ( ! $validated ) {
+				$new_settings[ $field_name ] = '';
+			} elseif ( isset( $settings[ $field_name ] ) ) {
+
+				// If the input is validated, choose a sanitizer
+				if ( is_int( $field_value ) ) {
+
+					// Use absint() as sanitizer if setting default is an integer
+					$sanitizer = 'absint';
+
+				} elseif ( is_string( $field_value ) ) {
+
+					// Use sanitize_text_field() as sanitizer if setting default is a string
+					$sanitizer = 'sanitize_text_field';
+				}
+
+				// Sanitize and save to new settings
+				if ( ! empty( $sanitizer ) ) {
+					$new_settings[ $field_name ] = call_user_func( $sanitizer, $settings[ $field_name ] );
+				}
+			}
 		}
 
-		$validated = self::validate_image_dimension( $settings['image_width'] );
-		if ( ! $validated ) {
-			$new_settings['image_width'] = '';
-		} elseif ( isset( $settings['image_width'] ) ) {
-			$new_settings['image_width'] = absint( $settings['image_width'] );
-		}
-
-		$validated = self::validate_image_dimension( $settings['image_height'] );
-		if ( ! $validated ) {
-			$new_settings['image_height'] = '';
-		} elseif ( isset( $settings['image_height'] ) ) {
-			$new_settings['image_height'] = absint( $settings['image_height'] );
-		}
-
+		// Return the new settings
 		return $new_settings;
 	}
 
 	/**
-	 * Validates image total.
+	 * Validates item total.
 	 *
 	 * @param $data to be validated
+	 * @param $label of field being validated
 	 * @return bool
 	 * @since 0.1.1
 	 */
-	public function validate_image_total( $data = null ) {
+	public function validate_item_total( $data = null, $label = '' ) {
+		$result     = true;
+		$show_error = true;
+		$message    = '';
+		$message   .= ( ! empty( $label ) ) ? self::$setting_titles[ $label ] . ': ' : '';
 
 		if ( empty( $data ) && '0' !== $data ) {
 			// Rejects empty data without an error message
-			return false;
+			$show_error = false;
+			$result     = false;
 
 		} elseif ( empty( absint( $data ) ) || is_float( $data + 0 ) || $data < 1 ) {
 			// Rejects non-numeric strings, zero, null, false, empty, floats, and negative numbers
-			add_settings_error(
-				'sba_settings',
-				esc_attr( 'settings_updated' ),
-				// translators: This is the error message for an invalid image dimension.
-				__( 'Invalid total. The default value will be used. Next time, please use a whole number greater than zero.', 'simple-before-and-after' ),
-				'error'
-			);
+			$result = false;
 
-			return false;
+			// translators: This is the error message for an invalid image dimension.
+			$message .= __( 'Invalid total. The default value will be used. Next time, please use a whole number greater than zero.', 'simple-before-and-after' );
 
 		} elseif ( $data > 50 ) {
 			// Rejects 9999 to ensure consistent hard crops
-			add_settings_error(
-				'sba_settings',
-				esc_attr( 'settings_updated' ),
-				// translators: This is the error message for an image dimension that is 9999.
-				__( 'The image total is too high. The default value will be used. Next time, please use a number that is 50 or less.', 'simple-before-and-after' ),
-				'error'
-			);
+			$result = false;
 
-			return false;
+			// translators: This is the error message for an image dimension that is 9999.
+			$message .= __( 'The image total is too high. The default value will be used. Next time, please use a number that is 50 or less.', 'simple-before-and-after' );
+
 		}
 
-		return true;
+		if ( ! $result && $show_error ) {
+			add_settings_error(
+				'sba_settings',
+				'item_total',
+				esc_attr( $message ),
+				'error'
+			);
+		}
+
+		return $result;
+	}
+
+	/**
+	 * Validates image width. Alias of validate_image_dimension.
+	 *
+	 * @param $params variable-number parameters
+	 * @since 0.1.1
+	 */
+	public function validate_image_width( ...$params ) {
+		return self::validate_image_dimension( ...$params );
+	}
+
+	/**
+	 * Validates image height. Alias of validate_image_dimension.
+	 *
+	 * @param $params variable-number parameters
+	 * @since 0.1.1
+	 */
+	public function validate_image_height( ...$params ) {
+		return self::validate_image_dimension( ...$params );
 	}
 
 	/**
 	 * Validates image dimension.
 	 *
 	 * @param $data to be validated
+	 * @param $label of field being validated
 	 * @return bool
 	 * @since 0.1.1
 	 */
-	public function validate_image_dimension( $data = null ) {
+	public function validate_image_dimension( $data = null, $label = '' ) {
+		$result     = true;
+		$show_error = true;
+		$message    = '';
+		$message   .= ( ! empty( $label ) ) ? self::$setting_titles[ $label ] . ': ' : '';
 
 		if ( empty( $data ) && '0' !== $data ) {
 			// Rejects empty data without an error message
-			return false;
+			$show_error = false;
+			$result     = false;
 
 		} elseif ( empty( absint( $data ) ) || is_float( $data + 0 ) || $data < 1 ) {
 			// Rejects non-numeric strings, zero, null, false, empty, floats, and negative numbers
-			add_settings_error(
-				'sba_settings',
-				esc_attr( 'settings_updated' ),
-				// translators: This is the error message for an invalid image dimension.
-				__( 'Invalid dimension. The default value will be used. Next time, please use a whole number greater than zero. To resize previously uploaded images, regenerate the thumbnails.', 'simple-before-and-after' ),
-				'error'
-			);
+			$result = false;
 
-			return false;
+			// translators: This is the error message for an invalid image dimension.
+			$message .= __( 'Invalid dimension. The default value will be used. Next time, please use a whole number greater than zero. To resize previously uploaded images, regenerate the thumbnails.', 'simple-before-and-after' );
 
 		} elseif ( 9999 === $data ) {
 			// Rejects 9999 to ensure consistent hard crops
-			add_settings_error(
-				'sba_settings',
-				esc_attr( 'settings_updated' ),
-				// translators: This is the error message for an image dimension that is 9999.
-				__( 'Invalid dimension. The default value will be used. Next time, please use a number that is not 9999. To resize previously uploaded images, regenerate the thumbnails.', 'simple-before-and-after' ),
-				'error'
-			);
+			$result = false;
 
-			return false;
+			// translators: This is the error message for an image dimension that is 9999.
+			$message .= __( 'Invalid dimension. The default value will be used. Next time, please use a number that is not 9999. To resize previously uploaded images, regenerate the thumbnails.', 'simple-before-and-after' );
+
 		}
 
-		return true;
+		if ( ! $result && $show_error ) {
+			add_settings_error(
+				'sba_settings',
+				'image_dimension',
+				esc_attr( $message ),
+				'error'
+			);
+		}
+
+		return $result;
+	}
+
+	/**
+	 * Validates before label. Alias of validate_label.
+	 *
+	 * @param $params variable-number parameters
+	 * @since 0.1.1
+	 */
+	public function validate_before_label( ...$params ) {
+		return self::validate_label( ...$params );
+	}
+
+	/**
+	 * Validates after label. Alias of validate_label.
+	 *
+	 * @param $params variable-number parameters
+	 * @since 0.1.1
+	 */
+	public function validate_after_label( ...$params ) {
+		return self::validate_label( ...$params );
+	}
+
+	/**
+	 * Validates text label.
+	 *
+	 * @param $data to be validated
+	 * @param $label of field being validated
+	 * @return bool
+	 * @since 0.1.1
+	 */
+	public function validate_label( $data = null, $label = '' ) {
+		$result     = true;
+		$show_error = true;
+		$message    = '';
+		$message   .= ( ! empty( $label ) ) ? self::$setting_titles[ $label ] . ': ' : '';
+
+		if ( empty( $data ) && '0' !== $data ) {
+			// Rejects empty data without an error message
+			$show_error = false;
+			$result     = false;
+
+		} elseif ( sanitize_text_field( $data ) !== $data ) {
+			// Rejects invalid text
+			$result = false;
+
+			// translators: This is the error message for an invalid text label.
+			$message .= __( 'Invalid text label. The default value will be used.', 'simple-before-and-after' );
+
+		}
+
+		if ( ! $result && $show_error ) {
+			add_settings_error(
+				'sba_settings',
+				'label',
+				esc_attr( $message ),
+				'error'
+			);
+		}
+
+		return $result;
 	}
 }
