9c274cc745c590010ecc0fad7bc2ecf3dd7f95a6
Emily Leffler Schulman <emily.lefflerschulman@umuc.edu>
2019-10-17 10:28:01 -0400

    Change variable name to fix JS bug in which image swapping broke.

--- src/js/frontend/simple-before-and-after.js
+++ src/js/frontend/simple-before-and-after.js
@@ -1,13 +1,14 @@
+var i, j;
 var parents = document.querySelectorAll( '.sba-grid-item' );
 
 // Toggle 'inactive' class on grid item contents
-parents.forEach( function( parent ) {
-	parent.addEventListener( 'click', function() {
-		this.childNodes.forEach( function( child ) {
-			child.classList.toggle( 'inactive' );
-		});
+for ( i = 0; i < parents.length; i++ ) {
+	parents[i].addEventListener( 'click', function() {
+		for ( j = 0; j < this.children.length; j++ ) {
+			this.children[j].classList.toggle( 'inactive' );
+		}
 	});
-});
+}
 
 // Add class for non-touch devices
 if ( ! ( 'ontouchstart' in document.documentElement ) ) {
